ed61374a55bab5e2a61ae7669781d1e0a6500961
Faisal Shaikh <faisal.shaikh@spiculestech.com>
2024-12-09 16:37:55 +0530

    prog1.c modified

--- prog1.c
+++ prog1.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+int add(int a, int b);
 
 int main()
 {
-printf("hello world");
+printf("hello world\n");
+printf("a+b=%d\n",add(5,10));
 return 0;
 }
+
+int add (int a,int b){
+	return a+b;
+}
+
+
